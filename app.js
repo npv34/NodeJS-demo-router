1d64ec5758471abf21b732627a2dc0251184b729
npv34 <phanvanluan611996@gmail.com>
2022-06-07 11:38:11 +0700

    demo nodejs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const qs = require('qs');
 
 let users = [
     {
@@ -18,9 +19,97 @@ let users = [
         email: 'tai3@gmial.com',
         phone: '090909090'
     },
+    {
+        name: 'Tai',
+        email: 'tai@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 2',
+        email: 'tai2@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 3',
+        email: 'tai3@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai',
+        email: 'tai@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 2',
+        email: 'tai2@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 3',
+        email: 'tai3@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai',
+        email: 'tai@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 2',
+        email: 'tai2@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 3',
+        email: 'tai3@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai',
+        email: 'tai@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 2',
+        email: 'tai2@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 3',
+        email: 'tai3@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai',
+        email: 'tai@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 2',
+        email: 'tai2@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 3',
+        email: 'tai3@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai',
+        email: 'tai@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 2',
+        email: 'tai2@gmial.com',
+        phone: '090909090'
+    },
+    {
+        name: 'Tai 3',
+        email: 'tai3@gmial.com',
+        phone: '090909090'
+    },
 ]
-
-console.log(users);
 
 function showListUser(data) {
     let html = '';
@@ -30,7 +119,10 @@ function showListUser(data) {
         html += `<td>${data[i].name}</td>`
         html += `<td>${data[i].email}</td>`
         html += `<td>${data[i].phone}</td>`
-        html += `<td><a href="users/delete?id=${i}" class="btn btn-danger">Delete</a></td>`
+        html += `<td>
+<a href="users/delete?id=${i}" class="btn btn-danger">Delete</a>
+<a href="users/update?id=${i}" class="btn btn-primary">Update</a>
+</td>`
         html += '</tr>';
     }
     return html;
@@ -52,10 +144,13 @@ const server = http.createServer(((req, res) => {
 
     // phân tích url
     const urlPath = url.parse(req.url, true);
+    console.log(urlPath)
+
     let queryString = urlPath.query;
 
     // dùng switch-case điều hướng  theo url và method của request
     // nó đóng vai trò là router
+    let index;
 
     switch (urlPath.pathname) {
         case '/':
@@ -93,18 +188,69 @@ const server = http.createServer(((req, res) => {
             }))
             break;
         case '/users/delete':
-            let index = queryString.id;
+            index = queryString.id;
             deleteUser(index);
-
+            console.log(users)
             // set lại location cho res để trình duyệt gọi lên 1 request khác
             res.writeHead(301, {
-                Location: "http://localhost:8081"
+                "Location": "http://localhost:8081"
             })
-
-            res.end()
+            res.end();
         break;
-    }
+        case '/users/update':
+            index = queryString.id;
+            let userUpdate = users[index];
 
+            if (req.method === 'GET') {
+                fs.readFile('./views/edit.html', 'utf8', ((err, data) => {
+                    if (err) {
+                        throw new Error(err.message)
+                    }
+
+                    data = data.replace('<input type="text" value="" name="name">', `<input type="text" value="${userUpdate.name}" name="name">`)
+                    data = data.replace('<input type="text" value="" name="email">', `<input type="text" value="${userUpdate.email}" name="email">`)
+                    data = data.replace('<input type="text" value="" name="phone">', `<input type="text" value="${userUpdate.phone}" name="phone">`)
+                    data = data.replace('<form action="/users/update" method="post">', `<form action="/users/update?id=${index}" method="post">`)
+                    res.writeHead(200,'success', {'Content-type': 'text/html'})
+                    res.write(data)
+
+                    res.end()
+                }))
+            } else {
+                let data = ''
+                req.on('data', chunk => {
+                    data += chunk
+                })
+
+                req.on('end', () => {
+                    let dataForm = qs.parse(data);
+                    userUpdate.name = dataForm.name;
+                    userUpdate.email = dataForm.email;
+                    userUpdate.phone = dataForm.phone;
+
+                    res.writeHead(301, {
+                        "Location": "http://localhost:8081"
+                    })
+                    res.end();
+                })
+            }
+            break;
+        case '/js/my.js':
+            fs.readFile('./js/my.js','utf8', ((err, data) => {
+                    if (err) {
+                        throw new Error(err.message)
+                    }
+
+                    res.writeHead(200,'success', {'Content-type': 'text/javascript'})
+                    res.write(data);
+                    res.end();
+                }
+            ))
+            break;
+        default:
+            res.end()
+
+    }
 }))
 
 server.listen(8081, 'localhost', () => {
---
+++ js/my.js
@@ -0,0 +1,1 @@
+document.getElementById('search-btn').style.color = 'red'
